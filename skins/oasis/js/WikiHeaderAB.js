a99e6ba3a72b0cc9fb8360183d9405273dffb3d2
VForge <bartosz.bentkowski@gmail.com>
2014-05-13 18:09:47 +0200

    CON-1073 | Changes for styling

--- skins/oasis/js/WikiHeaderAB.js
+++ skins/oasis/js/WikiHeaderAB.js
@@ -64,4 +64,61 @@ $(function () {
 
 	navigation.children().wrapAll('<section class="local-navigation-container"></section>');
 	navigation.insertAfter('#WikiaHeader');
+
+	// COLORS
+	(function(){
+		/*jshint multistr: true */
+		var linkColor = $('.wikia-button').css('background-color') || '#000',
+			backgroundColor = $('.WikiaPageBackground').css('background-color') || '#fff',
+			css = '.WikiHeaderV2 { \
+		background: $header-color-secondary; \
+	} \
+		.WikiHeaderV2 .wordmark.text a { \
+		color: $header-color-primary; \
+	} \
+ \
+	.WikiNavV2 .nav-item > a { \
+		background: $header-color-secondary; \
+		color: $header-color-primary; \
+	} \
+	.WikiNavV2 .nav-item > a:hover { \
+		background: $header-color-primary; \
+		color: $header-color-secondary; \
+	} \
+	.WikiNavV2 .nav-item:hover > a, .WikiNavV2 .nav-item.active > a { \
+		background: $header-color-primary; \
+		color: $header-color-secondary; \
+	} \
+	.WikiNavV2 .nav-item .submenu { \
+		background: $header-color-primary; \
+		color: $header-color-secondary; \
+	} \
+	.WikiNavV2 .nav-item .submenu .subnav-2a { \
+		border-bottom: 1px solid $header-color-secondary; \
+	} \
+	.WikiNavV2 .nav-item .submenu .subnav-2a svg polygon { \
+		fill: $header-color-secondary; \
+	} \
+	.WikiNavV2 .nav-item .submenu .see-all { \
+		border-top: 1px solid $header-color-secondary; \
+	} \
+	.WikiNavV2 .nav-item .submenu .see-all svg polygon { \
+		fill: $header-color-secondary; \
+	} \
+	'.replace(/\$header-color-primary/g, linkColor).replace(/\$header-color-secondary/g,
+					backgroundColor),
+			head = document.head || document.getElementsByTagName('head')[0],
+			style = document.createElement('style');
+
+		console.log(css);
+
+		style.type = 'text/css';
+		if (style.styleSheet){
+			style.styleSheet.cssText = css;
+		} else {
+			style.appendChild(document.createTextNode(css));
+		}
+
+		head.appendChild(style);
+	})();
 });
